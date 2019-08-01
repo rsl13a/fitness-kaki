249fb786fb4c662904359551e819db7e61aea2d4
flatpackcode2 <colinnoahpeter@gmail.com>
2019-08-01 22:29:10 +0800

    added events blueprint, events model and image helper for amazon s3

--- finalproject_api/__init__.py
+++ finalproject_api/__init__.py
@@ -6,7 +6,9 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 ## API Routes ##
 from finalproject_api.blueprints.users.views import users_api_blueprint
 from finalproject_api.blueprints.sessions.views import sessions_api_blueprint
+from finalproject_api.blueprints.events.views import events_api_blueprint
 
 
 app.register_blueprint(csrf.exempt(users_api_blueprint), url_prefix='/api/v1/users')
 app.register_blueprint(csrf.exempt(sessions_api_blueprint), url_prefix='/api/v1/sessions')
+app.register_blueprint(csrf.exempt(events_api_blueprint), url_prefix='/api/v1/events')
---
+++ utils/im_helpers.py
@@ -0,0 +1,27 @@
+from boto3 import boto3, botocore
+from config import S3_BUCKET, S3_KEY, S3_LOCATION, S3_SECRET
+from flask import Flask
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id = S3_KEY,
+    aws_secret_access_key=S3_SECRET,
+)
+
+def upload_t0_s3(file, bucket_name=S3_BUCKET, acl="public-read"):
+    try:
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            file.filename,
+            ExtraArgs={
+                "ACL":acl,
+                "ContentType":file.content_type
+            }
+        )
+    except Exception as e:
+        print("Something Happened: ", e)
+        return e
+    
+    return f"{S3_LOCATION}.{file.filename}"
+    
